cb50a282430ee2e082a82fa207214f20be87fb22
Aby Abraham <abyabrkal@gmail.com>
2019-03-30 12:36:39 +0400

    Rank completed. Particle 'Bubbles' added as BG.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,74 @@
-import React, { Component } from 'react';
-import Navigation from './components/header/Navigation';
-import Logo from './components/header/Logo';
-import ImageLinkForm from './components/body/ImageLinkForm';
-import './App.css';
+import React, { Component } from "react";
+import Particles from "react-particles-js";
+import Navigation from "./components/header/Navigation";
+import Logo from "./components/header/Logo";
+import ImageLinkForm from "./components/body/ImageLinkForm";
+import Rank from "./components/body/Rank";
+import "./App.css";
 
+const particleOptions = {
+  particles: {
+    number: {
+      value: 260,
+      density: {
+        enable: true
+      }
+    },
+    "color": {
+      "value": "random"
+    },
+    size: {
+      value: 4,
+      random: true,
+      anim: {
+        speed: 4,
+        size_min: 0.3
+      }
+    },
+    line_linked: {
+      enable: false
+    },
+    move: {
+      random: true,
+      speed: 1,
+      direction: "top",
+      out_mode: "out"
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "bubble"
+      },
+      onclick: {
+        enable: true,
+        mode: "repulse"
+      }
+    },
+    modes: {
+      bubble: {
+        distance: 250,
+        duration: 2,
+        size: 0,
+        opacity: 0
+      },
+      repulse: {
+        distance: 400,
+        duration: 4
+      }
+    }
+  }
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles' params={particleOptions} />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/*<FaceRecognition />*/}
       </div>
---
+++ src/components/body/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='light-purple f3'>
+                {'Tony, your current rank is ...'}
+            </div>
+            <div className='purple f1'>
+                {'#5'}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
